596356b4e38c6f4f730f82febb188bdf3b0a5d3a
Alessandro Veneziano <aveneziano536@gmail.com>
2022-01-25 17:20:08 +0100

    .

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,9 +1,16 @@
 import "./login.css"
 import React, { Component } from 'react'
+import { Navigate } from "react-router-dom";
+
+/* assets */
 import computerImg from '../../assets/imgs/img1.png'
+/* func Components*/
 import Input from '../../components/classComponent/Input/Input'
 import Button from '../../components/funcComponent/Button/Button'
+/*  */
 import Translator from "../../components/funcComponent/Translator/Translator"
+
+/* utils */
 import loginUtils from "../../utils/login"
 
 // i18n
@@ -16,6 +23,9 @@ class Login extends Component {
         super(props)
 
         this.state = {
+            navigateToForgotPassword: false,
+            navigateToHome: false,
+            navigateToRegistation: false,
             mouseX: 0,
             mouseY: 0,
             inputEmail: "",
@@ -24,7 +34,18 @@ class Login extends Component {
             wrongPassword: false
         }
     }
+    /*momentario*/
+    componentDidMount() {
+        let arr = [
+            {
+                mail: 'pippo@gmail.com',
+                password: '1234',
+            }
+        ]
+        localStorage.setItem("arrayOfUsers", JSON.stringify(arr))
 
+    }
+    /*  */
 
     handleMouseMove = (e) => {
         let mouseX = (((e.pageX / 7) * -1) + 100)
@@ -36,9 +57,8 @@ class Login extends Component {
     handleMouseLeave = () => {
         this.setState({ mouseX: 0, mouseY: 0 })
     }
-
+    /* verify credentials */
     verifyLogin = () => {
-
         let email = this.state.inputEmail
         let psw = this.state.inputPassword
 
@@ -60,17 +80,52 @@ class Login extends Component {
         this.setState({ wrongEmail: wrongEmail, wrongPassword: wrongPsw })
 
         if (loginUtils.validateEmail(email) && loginUtils.validatePassword(psw)) {
-            alert("login ok")
+            alert("login accettabile")
+            this.Login()
         }
-
-
-
     }
-    getMail = (e) => {
+    /* accesso alla home */
+    Login = () => {
+        let existingUsers = JSON.parse(localStorage.getItem("arrayOfUsers"));
+        console.log(existingUsers);
+        for (let x = 0; x < existingUsers.length; x++) {
+            if (this.state.inputEmail === existingUsers[x].mail && this.state.inputPassword === existingUsers[x].password) {
+                this.setState(
+                    {
+                        navigateToHome: true,
+                    }
 
+                )
+            }
+        }
+        if (!this.state.navigateToHome) {
+            alert('email o password errati');
+        }
+    }
+    /*  */
+    /* accesso screen ForgotPassword */
+    NavigateToForgotPassword = () => {
+        this.setState(
+            {
+                navigateToForgotPassword: true,
+            }
+
+        )
+    }
+    /* accesso screen registation */
+    NavigateToRegistation = () => {
+        this.setState(
+            {
+                navigateToRegistation: true,
+            }
+
+        )
+    }
+
+    /* setting login's password and email */
+    getMail = (e) => {
         this.setState({ inputEmail: e.target.value })
     }
-
     getPassword = (e) => {
         this.setState({ inputPassword: e.target.value })
     }
@@ -125,11 +180,41 @@ class Login extends Component {
                             />
 
 
-                            <p className='login-text'>{this.props.t("LOGIN.forgotUser")} <span className='login-link'>Username / Password?</span></p>
+                            <p className='login-text'>{this.props.t("LOGIN.forgotUser")}
+                                <span className='login-link'>Password?</span>
+                            </p>
                         </div>
-                        <p className='login-link login-footer'>{this.props.t("LOGIN.createAccount")}<i style={{ marginLeft: 15 }} className="fas fa-long-arrow-alt-right"></i></p>
+                        <p className='login-link login-footer' onClick={this.NavigateToRegistation}>
+                            {this.props.t("LOGIN.createAccount")}
+                            <i style={{ marginLeft: 15 }} className="fas fa-long-arrow-alt-right">
+                            </i>
+                        </p>
                     </div>
                 </div>
+                {
+                    this.state.navigateToHome === true &&
+                    <Navigate to="/" replace={true}
+                        state={
+                            {
+                                mail: this.state.inputEmail,
+                            }
+                        }
+                    />
+                }
+                {
+                    this.state.navigateToRegistation === true &&
+                    <Navigate to="/registration" replace={true} />
+                }
+                {
+                    this.state.navigateToForgotPassword === true &&
+                    <Navigate to="/forgot-password" replace={true} />
+                }
+
+
+
+
+
+
             </div>
 
 
