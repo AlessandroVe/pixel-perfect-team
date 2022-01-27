83157ff97735fd03ec23292400f0aa4761941403
FraPolInfo <francescopolizziinfo@gmail.com>
2022-01-27 10:26:58 +0100

    mancava una virgola funziona tutto

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -105,7 +105,7 @@ class Login extends Component {
         this.setState(
             {
                 navigateToHome: navigateFlag,
-                errorFlag: errorFlag
+                errorFlag: errorFlag,
                 user: user,
             }
         )
